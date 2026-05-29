1a935bd62f30b0244f59ab340580886ef335be1c
tantrus332 <tantrus332@gmail.com>
2026-05-29 21:35:08 +0400

    add missing import for User entity in BankCardServiceImplTest

--- src/test/java/com/tantrus332/bankcards/service/impl/BankCardServiceImplTest.java
+++ src/test/java/com/tantrus332/bankcards/service/impl/BankCardServiceImplTest.java
@@ -27,6 +27,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import com.tantrus332.bankcards.entity.User;
 
 @ExtendWith(MockitoExtension.class)
 class BankCardServiceImplTest {
